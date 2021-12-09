4a60d829711e519ddbe2b2162bdda93739bc9955
ElisaOppe <91607662+ElisaOppe@users.noreply.github.com>
2021-12-09 17:34:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+
+float funzione (int a){
+    int r=0;
+    float g=0, d=0;
+    float numeri[a];
+    for (int i=0;i<a;i++){
+        cin >> numeri[r];
+        d = numeri[r];
+        r++;
+        if(d>g){
+            g=d;
+        }else if((r==1)&&(d<0)){
+            g=d;
+        }
+    }
+    return g;
+}
